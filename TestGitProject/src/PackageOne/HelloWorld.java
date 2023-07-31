a0cc8b0eb8219224632b19de9068a1ac17645942
Stanley AMAECHI <stanley.amaechi@student.oulu.fi>
2023-07-31 15:11:21 +0300

    This is my first version

---
+++ TestGitProject/src/PackageOne/HelloWorld.java
@@ -0,0 +1,12 @@
+package PackageOne;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+
+	}
+
+}
